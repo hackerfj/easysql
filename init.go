d8637e12c2932162701766f37debcfd8cb53397f
付杰 <geekfj@gmail.com>
2019-12-29 12:59:42 +0800

    add readme file

--- init.go
+++ init.go
@@ -6,11 +6,12 @@ import (
 	"time"
 )
 
+// DB
 type DB struct {
 	conn *sql.DB
 }
 
-// 初始化创建连接
+//Open 初始化创建连接
 func Open(driverName string, username string, password string, ip string, port string, dbName string) *DB {
 	db, err := sql.Open(driverName, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, ip, port, dbName))
 	check(err)
@@ -20,27 +21,27 @@ func Open(driverName string, username string, password string, ip string, port s
 	return &DB{db}
 }
 
-// 设置连接池中的最大闲置连接数。
+//SetMaxIdleConn 设置连接池中的最大闲置连接数。
 func (db *DB) SetMaxIdleConn(n int) {
 	db.SetMaxIdleConn(n)
 }
 
-// 设置数据库的最大连接数量。
+//SetMaxOpenConn 设置数据库的最大连接数量。
 func (db *DB) SetMaxOpenConn(n int) {
 	db.SetMaxOpenConn(n)
 }
 
-// 设置连接的最大可复用时间。
+//SetConnMaxLifetime 设置连接的最大可复用时间。
 func (db *DB) SetConnMaxLifetime(d time.Duration) {
 	db.SetConnMaxLifetime(d)
 }
 
-// 设置是否开启DEBUG模式
+//SetDeBUG 设置是否开启DEBUG模式
 func (db *DB) SetDeBUG(b bool) {
 	isDebug = b
 }
 
-// 关闭连接
+//Close 关闭连接
 func (db *DB) Close() {
 	db.Close()
 }
